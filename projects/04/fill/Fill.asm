fe677ff4c1692bb9610a29b9eb2871f1ab28c2ca
TonyG <tonyzgao@gmail.com>
2022-03-09 18:24:53 +0000

    complete project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,45 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(START)
+@SCREEN
+D = A
+@CURRENT
+M = D
+
+(LOOP)
+// Branch based on keyboard value
+@KBD 
+D = M
+@color
+M = D
+@WRITE
+D;JEQ // White
+@color
+M = -1 // Black
+
+(WRITE)
+// Write to screen
+@color
+D = M
+@CURRENT
+A = M
+M = D
+
+// Increment pointer
+@CURRENT
+M = M + 1
+// Check if screen is filled
+D = M
+@KBD
+D = D - A
+@START
+D;JGT
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
