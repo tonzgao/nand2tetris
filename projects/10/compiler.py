178deace577a413f41b7eaf70f6bca5f2ea57655
TG <tonyzgao@gmail.com>
2022-03-26 15:30:33 +0000

    10 - complete tokenizer

--- projects/10/compiler.py
+++ projects/10/compiler.py
@@ -1,34 +1,80 @@
+import argparse
+
+keywords = set([
+  'class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void', 'true', 'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return'
+])
+symbols = set([
+  '{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~'
+])
+
 class JackTokenizer:
-  def __init__(self, f):
-    self.file = f
+  def __init__(self, filename):
+    with open(filename, 'r') as f:
+      self.text = f.read()
+    self.counter = 0
+    self.token = ''
+
+  def removeComments(self):
+    single_lines = self.text.split('//')
+    single_lines_removed = ''.join([
+      line.split('\n', 1)[-1] for line in single_lines
+    ])
+    all_comments = single_lines_removed.split('/**')
+    self.text = ''.join([
+      line.split('*/', 1)[-1] for line in all_comments
+    ])
 
   def hasMoreTokens(self):
-    pass
+    return self.counter < len(self.text) - 1
 
   def advance(self):
-    pass
+    current = self.text[self.counter]
+    self.counter += 1
+    if current in symbols:
+      self.token = current
+    if current.isspace():
+      return self.advance()
+    result = current
+    while not current.isspace() and current not in symbols:
+      current = self.text[self.counter]
+      if current in symbols:
+        break
+      result += current
+      self.counter += 1
+      if not self.hasMoreTokens():
+        break
+    self.token = result
 
   def tokenType(self):
-    pass
+    if self.token in symbols:
+      return 'symbol'
+    if self.token in keywords:
+      return 'keyword'
+    if self.token.isdigit():
+      return 'integerConstant'
+    if self.token[0] == '"':
+      return 'stringConstant'
+    return 'identifier'
 
   def keyWord(self):
-    pass
+    if self.token in keywords:
+      return self.token
 
   def symbol(self):
-    pass
+    return self.token
 
   def identifier(self):
-    pass
+    return self.token
 
   def intVal(self):
-    pass
+    return int(self.token)
 
   def stringVal(self):
-    pass
+    return self.token
 
 class CompilationEngine:
-  def __init__(self, infile, outfile):
-    self.tokenizer = JackTokenizer(infile)
+  def __init__(self, filename, outfile):
+    self.tokenizer = JackTokenizer(filename)
     self.file = outfile
 
   def compileClass(self):
@@ -76,6 +122,17 @@ class CompilationEngine:
 class JackAnalyzer:
   def __init__(self, filename: str):
     self.filename = filename
+    self.tokenizer = JackTokenizer(filename)
 
   def analyze(self):
-    pass
+    self.tokenizer.removeComments()
+    while self.tokenizer.hasMoreTokens():
+      self.tokenizer.advance()
+      print(self.tokenizer.tokenType(), self.tokenizer.token)
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Compiler')
+  parser.add_argument('filename', help='input file')
+  args = parser.parse_args()
+  analyzer = JackAnalyzer(args.filename)
+  analyzer.analyze()
